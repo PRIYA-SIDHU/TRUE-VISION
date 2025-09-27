a21aac0bbf48ed0ad01836a504ed780d5849a8fb
PRIYA-SIDHU <priyasidhu363@gmail.com>
2025-09-27 16:25:02 +0530

    added pages

--- frontend/src/components/layouts/dashboard.jsx
+++ frontend/src/components/layouts/dashboard.jsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Footer from "./footer.jsx";
+import ObjectDetection from "../shared/ObjectDetection.jsx";
+import FaceRecognition from "../shared/facerecognition.jsx";
+import CurrencyDetector from "../shared/currencydetector.jsx";
 
-import objectdetection from "../shared/objectdetection.jsx";
+import ColourDetection from "../shared/colourdetect.jsx";
+import Text from "../shared/text.jsx";
+
 
 const  Dashboard = ()=>{
     const location = useLocation();
@@ -14,11 +22,20 @@ const  Dashboard = ()=>{
       }, 50); // 100ms is usually good, you can try slightly higher if needed
     }
   }, [location]);
+  
+return <>
+   <Text></Text>
+  
+   <div id="object-detection">
+        <ObjectDetection />
+      </div>
+   <CurrencyDetector></CurrencyDetector>
+   <BookReader></BookReader>
+   <FaceRecognition></FaceRecognition>
+  <ColourDetection/>
+<Footer></Footer>
+</>
+};
 
-  return (
 
-    <div id = "object-detection">
-        <objectdetection></objectdetection>
-    </div>
-  );
-}
+export default Dashboard ;
